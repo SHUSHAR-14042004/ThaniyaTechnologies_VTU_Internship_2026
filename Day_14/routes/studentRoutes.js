179ae0d991d1f7418edb97b2417b1b07603f0cf0
Shushar Kulal <shusharkulal@gmail.com>
2026-03-11 20:20:58 +0530

    added to get student by id and added error handler

---
+++ Day_14/routes/studentRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+
+const {
+  getStudents,
+  getStudentById,
+  addStudent,
+  updateStudent,
+  deleteStudent
+} = require("../controllers/studentController");
+const validateStudent = require("../middleware/validateStudent");
+
+router.get("/students", getStudents);
+router.get("/students/:id", getStudentById);
+router.post("/students", validateStudent, addStudent);
+router.delete("/students/:id", deleteStudent);
+router.put("/students/:id", validateStudent, updateStudent);
+
+module.exports = router;
