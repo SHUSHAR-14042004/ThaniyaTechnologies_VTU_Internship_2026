11f14c4becf25a0efbddcdf370f3041ab5e4b026
Shushar Kulal <shusharkulal@gmail.com>
2026-03-19 22:00:48 +0530

    day 22

---
+++ Day_22/controllers/studentController.js
@@ -0,0 +1,124 @@
+const Student = require("../models/Student");
+
+
+exports.getStudents = async (req, res) => {
+  try {
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const sort = req.query.sort || "name";
+    const course = req.query.course;
+
+    const query = {};
+
+    // Filtering
+    if (course) {
+      query.course = course;
+    }
+
+    const total = await Student.countDocuments(query);
+
+    const students = await Student.find(query)
+      .sort(sort)
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    res.json({
+      total,
+      page,
+      limit,
+      data: students
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.addStudent = async (req, res) => {
+  try {
+    const student = new Student(req.body);
+    const savedStudent = await student.save();
+    res.status(201).json(savedStudent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteStudent = async (req, res) => {
+  try {
+
+    const student = await Student.findByIdAndDelete(req.params.id);
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.json({
+      message: "Student deleted successfully"
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateStudent = async (req, res) => {
+  try {
+
+    const student = await Student.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.json({
+      message: "Student updated successfully",
+      data: student
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getStudentById = async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.searchStudents = async (req, res) => {
+  try {
+
+    const { name } = req.query;
+
+    const students = await Student.find({
+      name: { $regex: name, $options: "i" }
+    });
+
+    if (students.length === 0) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.json(students);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
